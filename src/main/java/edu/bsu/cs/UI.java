a98bbfe96cd2e349443db78d16e514c5bd7d4aa4
Lord-Raymik <silas.johnson@bsu.edu>
2026-02-17 11:28:58 -0500

    Created UI class

---
+++ src/main/java/edu/bsu/cs/UI.java
@@ -0,0 +1,16 @@
+package edu.bsu.cs;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class UI extends Application {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void start (Stage primaryStage) {
+
+    }
+
+}

092e2839b67c581ad162cb7d2928d1402eb89b1e
LordRaymik <silasjohnson317@gmail.com>
2026-02-19 00:07:51 -0500

    Configured the GUI's looks

--- src/main/java/edu/bsu/cs/UI.java
+++ src/main/java/edu/bsu/cs/UI.java
@@ -1,14 +1,23 @@
 package edu.bsu.cs;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
+import java.lang.foreign.SegmentAllocator;
+
 public class UI extends Application {
+
+    private final Button searchButton = new Button("Search");
+    private final TextArea outputPanel = new TextArea();
+    private final TextField inputField = new TextField();
 
     public static void main(String[] args) {
         launch(args);
@@ -16,7 +25,9 @@ public class UI extends Application {
 
     @Override
     public void start(Stage primaryStage){
+        outputPanel.setEditable(false);
         configure(primaryStage);
+        configureSearchButton();
     }
 
     private void configure(Stage stage){
@@ -24,15 +35,43 @@ public class UI extends Application {
         stage.setScene(new Scene(createRoot()));
         stage.sizeToScene();
         stage.show();
+        stage.setMinHeight(stage.getHeight()*2);
+        stage.setMinWidth(stage.getWidth());
     }
 
     private Pane createRoot() {
-        VBox root = new VBox();
-        root.getChildren().addAll(
-                new Label("Enter Query:")
+        BorderPane root = new BorderPane();
+        VBox inputPanel = new VBox();
+
+
+        inputPanel.getChildren().addAll(
+                new Label("Enter Query:"),
+                inputField,
+                searchButton
         );
-        root.setAlignment(Pos.CENTER);
+        inputPanel.setAlignment(Pos.CENTER);
+        searchButton.setMinWidth(50.0);
+        inputPanel.setSpacing(10);
+
+        root.setLeft(inputPanel);
+        root.setCenter(outputPanel);
+        HBox.setHgrow(outputPanel, Priority.ALWAYS);
+        outputPanel.setMaxHeight(Double.MAX_VALUE);
+        BorderPane.setMargin(root.getLeft(), new Insets(0, 20, 0, 20));
+
         return root;
+    }
+
+    private void configureSearchButton() {
+        searchButton.setOnAction(event -> searchForRevisions());
+    }
+
+    private void searchForRevisions() {
+        String input = inputField.getText();
+
+        String output = WikipediaManager.searchForRevisions(input);
+
+        outputPanel.setText(output);
     }
 
 }
